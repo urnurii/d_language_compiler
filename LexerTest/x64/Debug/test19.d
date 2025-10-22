b50314dc341d0a2a075820ad54c2c5dfa78ad743
Nuriev Ilyas <www.urnurii@gmail.com>
2025-10-22 22:26:24 +0300

    feat: added test for val

---
+++ LexerTest/x64/Debug/test19.d
@@ -0,0 +1,40 @@
+void main()
+{
+    int i0  = 0;
+    int i1  = 1;
+    int i2  = -1;
+    int i3  = 2147483647;           
+    int i4  = -2147483648;          
+    int i5  = 0b1010;               
+    int i6  = 0b1101_0011;          
+    int i7  = 0755;                 
+    int i8  = 0xFF;                 
+    int i9  = 0xAB_CD_EF;           
+
+    float f0  = 0.0f;
+    float f1  = 1.23f;
+    float f2  = -4.56f;
+    float f3  = 123456.789f;
+    float f4  = .5f;                      
+    float f5  = 6.022e23f;
+    float f6  = -1.602e-19f;
+    float f7  = 1_234.567_89f;            
+    float f8  = 1.0e+38f;                 
+    float f9  = 1.0e-38f;                
+
+    double d0  = 0.0;
+    double d1  = 3.141592653589793;
+    double d2  = -2.718281828459045;
+    double d3  = 1.2345_6789;
+    double d4  = .125;
+    double d5  = 1.0e308;
+    double d6  = 1.0e-308;
+
+    real r0  = 0.0L;
+    real r1  = 2.99792458e8L;
+    real r2  = -6.62607015e-34L;
+    real r3  = 1.234_567L;
+    real r4  = .000001L;
+    real r5  = 1.0e4932L;
+    real r6  = 1.0e-4932L;
+}
