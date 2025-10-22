e21436eec4ddf89eb2c615936550fe7b3083e0ee
Komarov Daniil <komarow.daniil@gmail.com>
2025-10-22 23:14:21 +0300

    feat: new exe and results

--- LexerTest/x64/Debug/start.bat
+++ LexerTest/x64/Debug/start.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set files=test1.d test2.d test3.d test4.d test5.d test6.d test7.d test8.d test9.d test10.d test11.d test12.d test13.d test14.d test15.d test16.d test17.d test18.d
+set files=test1.d test2.d test3.d test4.d test5.d test6.d test7.d test8.d test9.d test10.d test11.d test12.d test13.d test14.d test15.d test16.d test17.d test18.d test19.d
 
 set counter=1
 for %%f in (%files%) do (
