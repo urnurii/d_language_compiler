b5a2ad2c7708b6a4baefb63d27a79e63f800245d
Nuriev Ilyas <www.urnurii@gmail.com>
2026-01-22 03:19:17 +0300

    feat: added Generating class fields from global/class declarations

--- full_parser/codegen_jvm.c
+++ full_parser/codegen_jvm.c
@@ -10,6 +10,27 @@ static char *CodegenBuildMethodDescriptor(const NType *return_type, const NParam
     return BuildJvmMethodDescriptor(return_type, params);
 }
 
+static int CodegenAddFieldDecls(jvmc_class *cls, NInitDeclList *decls, uint16_t access_flag) {
+    if (cls == NULL || decls == NULL) {
+        return 1;
+    }
+    for (int i = 0; i < decls->count; i++) {
+        NInitDecl *decl = decls->decls[i];
+        jvmc_field *field = NULL;
+        if (decl == NULL || decl->name == NULL || decl->jvm_descriptor == NULL) {
+            continue;
+        }
+        field = jvmc_class_get_or_create_field(cls, decl->name, decl->jvm_descriptor);
+        if (field == NULL) {
+            return 0;
+        }
+        if (!jvmc_field_add_flag(field, access_flag)) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
 int GenerateClassFiles(NProgram *root, SemanticContext *ctx) {
     const char *class_name = "Main";
     const char *out_file = "Main.class";
@@ -52,6 +73,44 @@ int GenerateClassFiles(NProgram *root, SemanticContext *ctx) {
         if (!jvmc_code_return_void(code)) {
             jvmc_class_destroy(cls);
             return 1;
+        }
+    }
+
+    if (root != NULL) {
+        NSourceItem *item = root->first_item;
+        while (item != NULL) {
+            switch (item->type) {
+                case SOURCE_ITEM_DECL:
+                    if (!CodegenAddFieldDecls(cls, item->value.decl.init_decls, JVMC_FIELD_ACC_PUBLIC)) {
+                        jvmc_class_destroy(cls);
+                        return 1;
+                    }
+                    break;
+                case SOURCE_ITEM_CLASS: {
+                    NClassDef *class_def = item->value.class_def;
+                    NClassMember *member = class_def ? class_def->members.first : NULL;
+                    while (member != NULL) {
+                        if (member->type == CLASS_MEMBER_FIELD) {
+                            uint16_t access = JVMC_FIELD_ACC_PUBLIC;
+                            if (member->access == ACCESS_PRIVATE) {
+                                access = JVMC_FIELD_ACC_PRIVATE;
+                            } else if (member->access == ACCESS_PROTECTED) {
+                                access = JVMC_FIELD_ACC_PROTECTED;
+                            }
+                            if (!CodegenAddFieldDecls(cls, member->value.field.init_decls, access)) {
+                                jvmc_class_destroy(cls);
+                                return 1;
+                            }
+                        }
+                        member = member->next;
+                    }
+                    break;
+                }
+                case SOURCE_ITEM_FUNC:
+                case SOURCE_ITEM_ENUM:
+                    break;
+            }
+            item = item->next;
         }
     }
 
