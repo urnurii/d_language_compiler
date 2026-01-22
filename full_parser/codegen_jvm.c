43243baef4c976f21714e742fab30f9439384798
Nuriev Ilyas <www.urnurii@gmail.com>
2026-01-22 03:15:12 +0300

    feat: added Creating a base class and writing a file

--- full_parser/codegen_jvm.c
+++ full_parser/codegen_jvm.c
@@ -1,7 +1,31 @@
 #include "codegen_jvm.h"
+#include "jvmc/jvmc.h"
 
 int GenerateClassFiles(NProgram *root, SemanticContext *ctx) {
+    const char *class_name = "Main";
+    const char *out_file = "Main.class";
+    jvmc_class *cls = NULL;
+    int ok = 1;
+
     (void)root;
     (void)ctx;
+
+    cls = jvmc_class_create(class_name, "java/lang/Object");
+    if (cls == NULL) {
+        return 1;
+    }
+    ok = jvmc_class_add_flag(cls, JVMC_CLASS_ACC_PUBLIC);
+    ok = ok && jvmc_class_add_flag(cls, JVMC_CLASS_ACC_SUPER);
+    if (!ok) {
+        jvmc_class_destroy(cls);
+        return 1;
+    }
+
+    if (!jvmc_class_write_to_file(cls, out_file)) {
+        jvmc_class_destroy(cls);
+        return 1;
+    }
+
+    jvmc_class_destroy(cls);
     return 0;
 }
