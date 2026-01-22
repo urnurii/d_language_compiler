13427ccc537d8b4cac1567c7279e09a1a72f03b5
Nuriev Ilyas <www.urnurii@gmail.com>
2026-01-22 15:05:03 +0300

    edit: edited 03 test

--- full_parser/final_test/test_03_short_circuit.d
+++ full_parser/final_test/test_03_short_circuit.d
@@ -1,6 +1,6 @@
-bool hi(bool a)
+bool hi(bool a, int b)
 {
-    writeln("hello");
+    writef("hello, %d", b);
     return a;
 }
 
@@ -9,27 +9,27 @@ void main()
     int number;
     readf("%d", number);
 
-    if (number > 10 && hi(true))
-        writeln("number > 10 && hi(true)");
+    if (number > 10 && hi(true, 1))
+        writeln("(1)number > 10 && hi(true)");
 
-    if (number > 5 && hi(true))
-        writeln("number > 5 && hi(true)");
+    if (number > 5 && hi(true, 2))
+        writeln("(2)number > 5 && hi(true)");
 
-    if (number > 10 || hi(true))
-        writeln("number > 10 || hi(true)");
+    if (number > 10 || hi(true, 3))
+        writeln("(3)number > 10 || hi(true)");
 
-    if (number > 5 || hi(true))
-        writeln("number > 5 || hi(true)");
+    if (number > 5 || hi(true, 4))
+        writeln("(4)number > 5 || hi(true)");
 
-    if (number > 10 && hi(false))
-        writeln("number > 10 && hi(false)");
+    if (number > 10 && hi(false, 5))
+        writeln("(5)number > 10 && hi(false)");
 
-    if (number > 5 && hi(false))
-        writeln("number > 5 && hi(false)");
+    if (number > 5 && hi(false, 6))
+        writeln("(6)number > 5 && hi(false)");
 
-    if (number > 10 || hi(false))
-        writeln("number > 10 || hi(false)");
+    if (number > 10 || hi(false, 7))
+        writeln("(7)number > 10 || hi(false)");
 
-    if (number > 5 || hi(false))
-        writeln("number > 5 || hi(false)");
+    if (number > 5 || hi(false, 8))
+        writeln("(8)number > 5 || hi(false)");
 }
