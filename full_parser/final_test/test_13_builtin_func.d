b64367736fd66390f7705c9298eed390f3797fe2
Nuriev Ilyas <www.urnurii@gmail.com>
2026-01-25 20:18:22 +0300

    feat: added test arrays in builtin

--- full_parser/final_test/test_13_builtin_func.d
+++ full_parser/final_test/test_13_builtin_func.d
@@ -28,5 +28,27 @@ void main()
     writeln(s);
 
     writef("FORMAT: i=%d f=%f c=%c s=%s\n", i, f, c, s);
+
+    writeln("ARRAY OUTPUT:");
+    int[] arr = [1, 2, 3, 4, 5];
+    writeln("arr = ", arr);
+    writeln("arr[1..4] = ", arr[1..4]);
+    writeln("arr[0..0] = ", arr[0..0]);
+    writeln("arr[0..arr.length] = ", arr[0..arr.length]);
+
+    int[3] arr2 = [7, 8, 9];
+    writeln("arr2 = ", arr2);
+    writeln("arr2[0..2] = ", arr2[0..2]);
+    writeln("arr2[1..3] = ", arr2[1..3]);
+
+    writeln("INPUT ARRAY:");
+    int[] arr_in = new int[5];
+    writeln("Enter 5 ints:");
+    for (int idx = 0; idx < 5; idx += 1) {
+        readf("%d", arr_in[idx]);
+    }
+    writeln("arr_in = ", arr_in);
+    writeln("arr_in[1..4] = ", arr_in[1..4]);
+
     writeln("DONE");
 }
